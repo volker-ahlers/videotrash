c60cb623d98221436a53f835f6285049b20a8354
Volker Ahlers <volker.ahlers.de@gmail.com>
2025-06-14 01:49:33 +0200

    emove unused comp

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, Signal, ViewChild, effect, inject } from '@angular/core';
+import {
+  Component,
+  Input,
+  Signal,
+  ViewChild,
+  effect,
+  inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MessageService } from '../services/message.service';
@@ -10,35 +17,31 @@ import Hls from 'hls.js';
 @Component({
   selector: 'app-video',
   standalone: true,
-  imports: [
-    CommonModule,
-    VideoComponent,
-    MatButtonModule
-  ],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './video.component.html',
-  styleUrls: ['./video.component.scss']
+  styleUrls: ['./video.component.scss'],
 })
 export class VideoComponent {
   @ViewChild('hlsvideo', { static: true }) hlsvideo!: any;
 
   messageService: MessageService = inject(MessageService);
 
-  @Input() url: string = "";
-  @Input() codec: string = "video/mp4";
+  @Input() url: string = '';
+  @Input() codec: string = 'video/mp4';
 
   // gate or cockpit view
-  @Input() view: string = "";
+  @Input() view: string = '';
 
   // sensor label for cockpit view
-  @Input() sensor_label: string = "";
+  @Input() sensor_label: string = '';
 
   streams: Signal<IStream[]> = this.messageService.getStreamsSig();
   sensorLabel: Signal<string> = this.messageService.getSensorLabelSig();
-  selectedMessage: Signal<IMessage | null> = this.messageService.getSelectedMessageSig();
+  selectedMessage: Signal<IMessage | null> =
+    this.messageService.getSelectedMessageSig();
 
   // set currenttime
   currenttime: number = 0;
-
 
   //timeout for video player
   timeouttime: number = 300;
@@ -51,15 +54,24 @@ export class VideoComponent {
     and updates the url for the video player
     */
     effect(() => {
-
-      if (this.view !== "gate") {
+      if (this.view !== 'gate') {
         return;
       }
 
-      console.warn("effect", this.constructor.name, this.view, this.sensor_label);
+      console.warn(
+        'effect',
+        this.constructor.name,
+        this.view,
+        this.sensor_label
+      );
 
-      if (this.selectedMessage()?.sensor_label !== "") {
-        this.debug && console.log("this.selectedMessage()", this.selectedMessage()?.sensor_label, this.sensor_label);
+      if (this.selectedMessage()?.sensor_label !== '') {
+        this.debug &&
+          console.log(
+            'this.selectedMessage()',
+            this.selectedMessage()?.sensor_label,
+            this.sensor_label
+          );
         if (this.selectedMessage()?.sensor_label === this.sensor_label) {
           const time = this.selectedMessage()?.frame_id ?? 0;
           if (time) {
@@ -70,32 +82,39 @@ export class VideoComponent {
     });
   }
 
-
   ngOnInit() {
-    this.debug && console.log("init video", this.sensor_label);
+    this.debug && console.log('init video', this.sensor_label);
     this.setUrl(this.sensor_label);
     this.sethlsSupport();
   }
 
   /*
-  * sets the url for the video player from the config
-  * selects the url for cockpit- or gate-view
-  */
+   * sets the url for the video player from the config
+   * selects the url for cockpit- or gate-view
+   */
   setUrl(label: string) {
     const stream = this.streams().filter((stream) => stream.name === label)[0];
     if (stream !== undefined) {
       this.debug && console.log(label, stream.name, stream);
-      this.url = this.view === 'cockpit' ? stream.user_stream_dir_raw : stream.user_stream_dir_detection;
+      this.url =
+        this.view === 'cockpit'
+          ? stream.user_stream_dir_raw
+          : stream.user_stream_dir_detection;
     }
   }
 
   /*
-  * ngAfterViewInit is necessary to initialize the video player as a viewchild
-  * initializes the hls.js player
-  * sets some default values for the video player
-  */
+   * ngAfterViewInit is necessary to initialize the video player as a viewchild
+   * initializes the hls.js player
+   * sets some default values for the video player
+   */
   ngAfterViewInit() {
-    this.debug && console.log("init video ngAfterViewInit", this.hlsvideo, this.hlsvideo.nativeElement);
+    this.debug &&
+      console.log(
+        'init video ngAfterViewInit',
+        this.hlsvideo,
+        this.hlsvideo.nativeElement
+      );
 
     this.hlsvideo.nativeElement.muted = true;
     this.hlsvideo.nativeElement.autoplay = true;
@@ -104,35 +123,43 @@ export class VideoComponent {
   }
 
   /*
-  * initializes the hls.js player
-  */
+   * initializes the hls.js player
+   */
   sethlsSupport() {
-    this.debug && console.log("HLS", this.url);
+    this.debug && console.log('HLS', this.url);
     if (this.url.endsWith('.m3u8')) {
       if (Hls.isSupported()) {
         var hls = new Hls();
-        this.debug && console.log("isSupported");
+        this.debug && console.log('isSupported');
         hls.loadSource(this.url);
         if (this.hlsvideo) {
           hls.attachMedia(this.hlsvideo.nativeElement);
-          this.debug && console.log("attachMedia done");
+          this.debug && console.log('attachMedia done');
         }
-      }
-      else if (this.hlsvideo.canPlayType('application/vnd.apple.mpegurl')) {
-        console.log("notSupported");
+      } else if (this.hlsvideo.canPlayType('application/vnd.apple.mpegurl')) {
+        console.log('notSupported');
         this.hlsvideo.src = this.url;
       }
     }
   }
 
   /*
-  * to avoid conflicts uit checks if the video is paused or ended
-  * and sets a timeout to play the video
-  */
+   * to avoid conflicts uit checks if the video is paused or ended
+   * and sets a timeout to play the video
+   */
   playVideo() {
-    console.log("this.hlsvideo.nativeElement.paused", this.hlsvideo.nativeElement.paused);
-    console.log("this.hlsvideo.nativeElement.ended", this.hlsvideo.nativeElement.ended);
-    if (this.hlsvideo?.nativeElement && (this.hlsvideo.nativeElement.paused || this.hlsvideo.nativeElement.ended)) {
+    console.log(
+      'this.hlsvideo.nativeElement.paused',
+      this.hlsvideo.nativeElement.paused
+    );
+    console.log(
+      'this.hlsvideo.nativeElement.ended',
+      this.hlsvideo.nativeElement.ended
+    );
+    if (
+      this.hlsvideo?.nativeElement &&
+      (this.hlsvideo.nativeElement.paused || this.hlsvideo.nativeElement.ended)
+    ) {
       setTimeout(() => {
         this.sethlsSupport();
         this.currenttime = 0;
@@ -143,23 +170,29 @@ export class VideoComponent {
   }
 
   /*
-  * should be used to jump to a specific time in the video
-  */
+   * should be used to jump to a specific time in the video
+   */
   jumpToTime(time: number) {
     this.currenttime = time;
     setTimeout(() => {
-      this.debug && console.log("label", this.selectedMessage()?.sensor_label, this.sensor_label);
+      this.debug &&
+        console.log(
+          'label',
+          this.selectedMessage()?.sensor_label,
+          this.sensor_label
+        );
       this.hlsvideo.nativeElement.currentTime = time;
-      console.log("currentTime", this.hlsvideo.nativeElement.currentTime, time);
+      console.log('currentTime', this.hlsvideo.nativeElement.currentTime, time);
       this.hlsvideo.nativeElement.pause();
-    }, (100 + this.timeouttime));
+    }, 100 + this.timeouttime);
   }
 
   /*
-  * should take the movie to the life point means the current frame
-  * test if this works
-  */
-  resume() {  //TODO check
+   * should take the movie to the life point means the current frame
+   * test if this works
+   */
+  resume() {
+    //TODO check
     if (environment.production) {
       this.hlsvideo.nativeElement.load();
       this.playVideo();
@@ -167,6 +200,12 @@ export class VideoComponent {
   }
 
   ngOnDestroy() {
-    this.debug && console.warn("destroyed", this.constructor.name, this.view, this.sensor_label);
+    this.debug &&
+      console.warn(
+        'destroyed',
+        this.constructor.name,
+        this.view,
+        this.sensor_label
+      );
   }
 }
