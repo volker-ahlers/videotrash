c75cee4a10df2abb2acbb0feecd65b8fd0dc50c9
Volker Ahlers <volker.ahlers.de@gmail.com>
2025-06-16 22:13:24 +0200

    changes

--- src/app/messages/message.component.spec.ts
+++ src/app/messages/message.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { MessageComponent } from './message.component';
 
 describe('MessageComponent', () => {
@@ -8,7 +9,11 @@ describe('MessageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [MessageComponent]
+      imports: [MessageComponent],
+      providers: [
+        provideHttpClient(), // HttpClient bereitstellen
+        provideHttpClientTesting(), // Test-Umgebung für HttpClient bereitstellen
+      ],
     });
     fixture = TestBed.createComponent(MessageComponent);
     component = fixture.componentInstance;
@@ -17,5 +22,30 @@ describe('MessageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return "ok", if confidence > 0.9', () => {
+    // Arrange
+    component.message.object = { confidence: 0.95 } as any; // Mock the object structure
+    // Act
+    const result = component.getObjectColor();
+    // Assert
+    expect(result).toBe('ok');
+  });
+  it('should return "alarm", if confidence =< 0.9', () => {
+    // Arrange
+    component.message.object = { confidence: 0.9 } as any; // Mock the object structure
+    // Act
+    const result = component.getObjectColor();
+    // Assert
+    expect(result).toBe('alarm');
+  });
+  it('should return "alarm", if confidence < 0.9', () => {
+    // Arrange
+    component.message.object = { confidence: 0.8 } as any; // Mock the object structure
+    // Act
+    const result = component.getObjectColor();
+    // Assert
+    expect(result).toBe('alarm');
   });
 });
--- src/app/more/directives/active-msg-cnt.directive.spec.ts
+++ src/app/more/directives/active-msg-cnt.directive.spec.ts
@@ -2,7 +2,10 @@ import { ActiveMsgCntDirective } from './active-msg-cnt.directive';
 
 describe('ActiveMsgCntDirective', () => {
   it('should create an instance', () => {
-    const directive = new ActiveMsgCntDirective();
+    const mockElementRef = {
+      nativeElement: document.createElement('div'),
+    } as any;
+    const directive = new ActiveMsgCntDirective(mockElementRef);
     expect(directive).toBeTruthy();
   });
 });
